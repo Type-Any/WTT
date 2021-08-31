c3dfca7e953dcb78ebea2c32ada62eb10df79f58
Dal Yoon <yeodal89@gmail.com>
2021-08-31 10:46:18 +0900

    ADD global props

--- App.tsx
+++ App.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Router from './src/routers';
 import ApiProvider from './src/contexts/Api';
 import NavProvider from './src/contexts/Nav';
+import {setCustomTouchableOpacity} from 'react-native-global-props';
+
+setCustomTouchableOpacity({
+  activeOpacity: 0.6,
+});
 
 const App = () => {
   const onApiError = (error: any) => {
